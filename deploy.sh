88dc757601d1b3faa16015690b431ab6d0da0080
Ann Catherine Jose <Ann_Jose@intuit.com>
2017-10-28 22:05:46 -0700

    Adjusted the weights to fix an ordering issue in left menu. Made blackburn the default theme. Updated the deploy script to use the default theme.
    Also updated the git submodule in public (since the site with these changes was regenerated and published 5 min ago)

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo "    [deploy] Started Deployment to github..."
 
 # Build the project. 
 # hugo -t redlounge  --buildDrafts # if using a theme, replace by `hugo -t <yourtheme>`
 
-hugo -t blackburn  # This will build only posts/pages that are ready to publish (i.e. draft=false in the md file)
+echo "    [deploy] Generating static site to 'public' folder...\n"
+hugo  # This will build posts/pages using the theme specified in config.toml that are ready to publish (i.e. draft=false in the md file)
+
+echo ""
 
 # Go To Public folder
 cd public
 # Add changes to git.
 git add -A
 
+echo "    [deploy] Committing the changes to local repo..."
 # Commit changes.
-msg="rebuilding site `date`"
+# Note: DO NOT remove the spaces on both sides of = operator (otherwise bash will consider it as two comamnds)
+msg="Rebuilt the site and publishing to GitHub `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
 # Push source and build repos.
+echo "    [deploy] Pushing the changes to remote..."
 git push origin master
 
 # Come Back
 cd ..
+
+echo "    [deploy] Done Deployment to github..."
